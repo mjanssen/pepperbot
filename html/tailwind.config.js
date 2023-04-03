1107a782ef4cf39548f616ab94d962819d1de9ce
Glenn de Haan <glenn@dehaan.cloud>
2023-04-03 22:36:31 +0200

    Initial setup of homepage

---
+++ html/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mode: 'jit',
+    content: ['./public/**/*.html', './public/*.html'],
+    darkMode: 'media',
+    theme: {},
+    variants: {
+        extend: {}
+    },
+    plugins: []
+};
