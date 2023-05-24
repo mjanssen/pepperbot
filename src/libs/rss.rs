b1f0ab571fed2f4817d95c7a4cc78ce7c7b821a5
mjanssen <dev.marnix@gmail.com>
2023-05-24 11:21:05 +0200

    Updated request duration and message queuing

--- src/libs/rss.rs
+++ src/libs/rss.rs
@@ -1,3 +1,4 @@
+use reqwest::header::{ACCEPT, ACCEPT_LANGUAGE, USER_AGENT};
 use rss::Channel;
 use thiserror::Error;
 
@@ -16,10 +17,25 @@ pub enum RSSError {
 }
 
 pub async fn get_rss_data() -> Result<Channel, RSSError> {
-    let feed = reqwest::get(RSS_URL).await?.bytes().await?;
+    let client = reqwest::Client::new();
+    let feed = client
+        .get(RSS_URL)
+        .header(USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
+        .header(ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+        .header(ACCEPT_LANGUAGE, "en-GB,en;q=0.6")
+        .header("sec-ch-ua", "\"Brave\";v=\"113\", \"Chromium\";v=\"113\", \"Not-A.Brand\";v=\"24\"")
+        .header("sec-ch-ua-mobile", "?0")
+        .header("sec-ch-ua-platform", "\"macOS\"")
+        .header("sec-fetch-site", "none")
+        .header("sec-fetch-mod", "")
+        .header("sec-fetch-user", "?1")
+        .header("sec-fetch-mode", "navigate")
+        .send()
+        .await?
+        .bytes()
+        .await?;
 
     let channel = Channel::read_from(&feed[..])?;
 
     Ok(channel)
 }
-
