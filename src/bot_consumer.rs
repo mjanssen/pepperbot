7d01efea4e28279454a69a2605bec5a16b74234b
Glenn de Haan <glenn@dehaan.cloud>
2023-06-13 23:38:37 +0200

    Merged redis files into one. Removed old redis_stream_client.rs

--- src/bot_consumer.rs
+++ src/bot_consumer.rs
@@ -1,20 +1,19 @@
 pub mod libs;
 pub mod structs;
 
+use libs::variable::get_environment_variable;
+use libs::version::print_version;
 use libs::redis::Database;
-use libs::redis_stream_client::RedisStreamClient;
 use libs::telegram::BotMessageService;
 use log::{error, info};
 use redis::ConnectionLike;
-use std::env;
 use thiserror::Error;
 
 use teloxide::Bot;
 
 use regex::Regex;
 
-use crate::libs::redis::{get_config, get_subscribers, increase_config_value};
-use crate::libs::version::print_version;
+use crate::libs::redis::{get_config, get_subscribers, increase_config_value, create_generic_config, read_message};
 
 #[derive(Debug, Error)]
 enum ConsumerError {
@@ -29,117 +28,112 @@ async fn main() -> Result<(), ConsumerError> {
 
     info!("Starting bot consumer service");
 
+    let redis_url = get_environment_variable("REDIS_URL");
     let sanitize_regex = Regex::new(r"([^\w\s\\'\\’\\$\\€])").unwrap();
 
-    if let Ok(redis_domain) = env::var("REDIS_URL") {
-        match redis::Client::open(redis_domain.clone()) {
-            Ok(redis_client) => {
-                let stream_client = RedisStreamClient {
-                    client: redis_client.clone(),
-                };
+    match redis::Client::open(redis_url.clone()) {
+        Ok(redis_client) => {
+            // Make sure we have the required configuration available
+            let _ = create_generic_config(redis_client.clone());
 
-                // Make sure we have the required configuration available
-                let _ = stream_client.create_generic_config();
+            let bot_service = BotMessageService {
+                bot: Bot::from_env(),
+            };
 
-                let bot_service = BotMessageService {
-                    bot: Bot::from_env(),
-                };
+            loop {
+                if redis_client.is_open() == false {
+                    panic!("Redis connection dropped");
+                }
 
-                loop {
-                    if redis_client.is_open() == false {
-                        panic!("Redis connection dropped");
-                    }
+                if let Ok(mut con) = redis_client.get_connection() {
+                    if let Some(message) = read_message(&mut con) {
+                        info!("{}", message.id);
 
-                    if let Ok(mut con) = redis_client.get_connection() {
-                        if let Some(message) = stream_client.read(&mut con) {
-                            info!("{}", message.id);
+                        // Make sure we're using the message database
+                        let _: Result<(), redis::RedisError> = redis::cmd("SELECT")
+                            .arg(Database::MESSAGE as u8)
+                            .query(&mut con);
 
-                            // Make sure we're using the message database
-                            let _: Result<(), redis::RedisError> = redis::cmd("SELECT")
-                                .arg(Database::MESSAGE as u8)
-                                .query(&mut con);
+                        let res: i64 = redis::cmd("EXISTS").arg(&message.id).query(&mut con)?;
 
-                            let res: i64 = redis::cmd("EXISTS").arg(&message.id).query(&mut con)?;
+                        // Only send if the message has not been send yet
+                        if res == 1 {
+                            continue;
+                        }
 
-                            // Only send if the message has not been send yet
-                            if res == 1 {
-                                continue;
-                            }
+                        // Store this message in Redis to make sure it doesn't get
+                        // queued again
+                        let _: Result<(), redis::RedisError> =
+                            redis::cmd("SET").arg(&message.id).arg(1).query(&mut con);
 
-                            // Store this message in Redis to make sure it doesn't get
-                            // queued again
-                            let _: Result<(), redis::RedisError> =
-                                redis::cmd("SET").arg(&message.id).arg(1).query(&mut con);
+                        // Set expiration for key - 2 days
+                        let _: Result<(), redis::RedisError> = redis::cmd("EXPIRE")
+                            .arg(&message.id)
+                            .arg(172800)
+                            .query(&mut con);
 
-                            // Set expiration for key - 2 days
-                            let _: Result<(), redis::RedisError> = redis::cmd("EXPIRE")
-                                .arg(&message.id)
-                                .arg(172800)
-                                .query(&mut con);
+                        // Check if the bot has been disabled by the admin
+                        let is_operational: String = get_config(
+                            &mut con,
+                            libs::redis::Config::OperationalKey,
+                            Database::MESSAGE,
+                        )
+                        .unwrap_or("1".to_string());
 
-                            // Check if the bot has been disabled by the admin
-                            let is_operational: String = get_config(
+                        // Only send messages and get subs when we're operational
+                        if is_operational.eq(&"1") {
+                            let _ = increase_config_value::<()>(
                                 &mut con,
-                                libs::redis::Config::OperationalKey,
+                                libs::redis::Config::DealsSentKey,
                                 Database::MESSAGE,
-                            )
-                            .unwrap_or("1".to_string());
+                                1,
+                            );
 
-                            // Only send messages and get subs when we're operational
-                            if is_operational.eq(&"1") {
-                                let _ = increase_config_value::<()>(
-                                    &mut con,
-                                    libs::redis::Config::DealsSentKey,
-                                    Database::MESSAGE,
-                                    1,
-                                );
+                            info!("Sending message {:?}", &message);
 
-                                info!("Sending message {:?}", &message);
+                            let subscribers = get_subscribers(redis_client.clone()).await;
+                            if let Ok(subs) = subscribers {
+                                let mut messages_sent = 0;
 
-                                let subscribers = get_subscribers(redis_client.clone()).await;
-                                if let Ok(subs) = subscribers {
-                                    let mut messages_sent = 0;
-
-                                    for (chat_id, categories) in subs {
-                                        // If user did not subscribe for this category, bail
-                                        if let Some(c) = categories {
-                                            if c.contains(&message.payload.category) == false {
-                                                continue;
-                                            }
+                                for (chat_id, categories) in subs {
+                                    // If user did not subscribe for this category, bail
+                                    if let Some(c) = categories {
+                                        if c.contains(&message.payload.category) == false {
+                                            continue;
                                         }
-
-                                        let sanitized_title = sanitize_regex
-                                            .replace_all(message.payload.title.as_str(), "\\$1");
-
-                                        info!("Sending {} to {}", message.payload.link, chat_id);
-
-                                        let _ = bot_service
-                                            .send_message(
-                                                chat_id,
-                                                format!(
-                                                    "[{}]({})",
-                                                    sanitized_title, message.payload.link
-                                                ),
-                                            )
-                                            .await;
-
-                                        messages_sent += 1;
                                     }
 
-                                    let _ = increase_config_value::<()>(
-                                        &mut con,
-                                        libs::redis::Config::MessagesSentKey,
-                                        Database::MESSAGE,
-                                        messages_sent,
-                                    );
+                                    let sanitized_title = sanitize_regex
+                                        .replace_all(message.payload.title.as_str(), "\\$1");
+
+                                    info!("Sending {} to {}", message.payload.link, chat_id);
+
+                                    let _ = bot_service
+                                        .send_message(
+                                            chat_id,
+                                            format!(
+                                                "[{}]({})",
+                                                sanitized_title, message.payload.link
+                                            ),
+                                        )
+                                        .await;
+
+                                    messages_sent += 1;
                                 }
+
+                                let _ = increase_config_value::<()>(
+                                    &mut con,
+                                    libs::redis::Config::MessagesSentKey,
+                                    Database::MESSAGE,
+                                    messages_sent,
+                                );
                             }
                         }
                     }
                 }
             }
-            Err(_) => error!("Connection with Redis failed"),
         }
+        Err(_) => error!("Connection with Redis failed"),
     }
 
     Ok(())
--- src/libs/mod.rs
+++ src/libs/mod.rs
@@ -1,6 +1,6 @@
 pub mod rss;
 pub mod telegram;
 pub mod redis;
-pub mod redis_stream_client;
 pub mod category;
+pub mod variable;
 pub mod version;
